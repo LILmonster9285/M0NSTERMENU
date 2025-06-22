ed613978eaae7188e0ce5d9ce4ceafbbe9bb3999
LILmonster9285 <gagefemail@gmail.com>
2025-06-21 20:59:15 -0500

    INSTALLER.bat

--- INSTALLER.bat
+++ INSTALLER.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
 
-
+:: Prompt user
 set /p USERINPUT=Do you wish to install? (y/n): 
 if /i "%USERINPUT%"=="y" goto install
 if /i "%USERINPUT%"=="n" goto end
 echo Invalid input.
 goto end
 
-
+:install
 set "GORILLA_TAG_PATH=C:\Program Files (x86)\Steam\steamapps\common\Gorilla Tag"
 set "BEPINEX_URL=https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip"
 set "MODMENU_URL=https://github.com/LILmonster9285/M0NSTERMENU/releases/download/v1.0.0/M0NSTERMENU.dll"
@@ -17,7 +17,7 @@ echo.
 echo Installing M0NSTERMENU and BepInEx...
 cd /d "%~dp0"
 
-
+:: === DOWNLOAD BEPINEX ===
 echo Downloading BepInEx...
 curl -L -o bepinex.zip "%BEPINEX_URL%"
 if errorlevel 1 (
@@ -25,14 +25,15 @@ if errorlevel 1 (
     goto end
 )
 
-
+:: === EXTRACT BEPINEX ===
 echo Extracting BepInEx...
 powershell -Command "Expand-Archive -Force 'bepinex.zip' '%TEMP%\bepinex'"
 
-
+:: === COPY TO GAME ===
 echo Installing BepInEx into Gorilla Tag...
 xcopy /E /I /Y "%TEMP%\bepinex\*" "%GORILLA_TAG_PATH%\"
 
+:: === DOWNLOAD MOD MENU ===
 echo Downloading M0NSTERMENU...
 curl -L -o "%GORILLA_TAG_PATH%\BepInEx\plugins\M0NSTERMENU.dll" "%MODMENU_URL%"
 if errorlevel 1 (
@@ -41,7 +42,7 @@ if errorlevel 1 (
 )
 
 echo.
-echo Installation complete!
+echo ✅ Installation complete!
 pause
 goto end
 
